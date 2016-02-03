7838fd6b026226ef6a66fc92c6455a34ffa39aa9
nelabdiel <nel.abdiel@gmail.com>
2016-02-03 13:45:23 -0500

    pickling is no fun

--- IoKwb.py
+++ IoKwb.py
@@ -46,8 +46,9 @@ class Company1(object):
     def __init__(self, mod):
         self.mod = mod
 with open('nei_model.pkl', 'rb') as input:
-    nei_mod = pickle.load(input)    
-nei_model_predict = nei_mod.mod
+    nei_model_predict = pickle.load(input)    
+
+nei_model_predict = nei_model_predict.mod
 
 
 #Nel-Sinclair Curve
